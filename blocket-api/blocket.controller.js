1ae0a3cd0821658895f1ac4c7858e4390f1260bd
markusaverheim <markus.averheim@gmail.com>
2019-08-06 20:33:44 +0200

    Refactor: Move html parsing to parsing service

--- blocket-api/blocket.controller.js
+++ blocket-api/blocket.controller.js
@@ -2,106 +2,34 @@ const express = require('express');
 const router = express.Router();
 const blocketService = require('./blocket.service');
 const url = require('url');
-const queryString = require('query-string');
+
+const parser = require('./parsing.service');
 
 router.get('/regions', getRegions);
 router.get('/search', search);
 
 module.exports = router;
 
+
 async function getRegions(request, response) {
-    const regions = [];
     blocketService.getDocument('https://www.blocket.se').then(document => {
-        console.log(document);
-        const areas = document.getElementsByClassName('region_link_list');
-
-        for (i = 0; i < areas.length; i++) {
-            region = areas[i];
-
-            regions.push({
-                'name': region.text,
-                'href': region.getAttribute('href'),
-                'regionNumber': region.getAttribute('data-region')
-            })
-        }
+        const regions = parser.parseRegions(document);
 
         response.send(regions);
-
-    }).catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
 }
 
 function search(request, response) {
-    path = buildPath(request);
-    console.log(path);
+    const path = buildPath(request);
 
     blocketService.getDocument(path).then(document => {
-        const ads = getAds(document);
-        const pagination = getPagination(document, request);
+        const ads = parser.parseAds(document);
+        const pagination = parser.parsePagination(document, request);
 
         response.send({ ads, pagination });
-    }).catch((error) => console.log(error));
-}
-
-function getAds(document) {
-    const ads = [];
-    const itemList = document.getElementById('item_list');
-    if (itemList) {
-        const articles = itemList.getElementsByTagName('article');
-        
-        for (let i = 0; i < articles.length; i++) {
-            const article = articles[i];
-            const price = article.getElementsByClassName('list_price')[0].textContent;
-            const date = article.getElementsByTagName('time')[0].textContent;
-            const header = article.getElementsByTagName('h1')[0].getElementsByTagName('a')[0];
-            const title = header.getAttribute('title');
-            const url = header.getAttribute('href');
-
-            const image = article.getElementsByTagName('img')[0];
-            const imageSrc = image ? image.getAttribute('longdesc') || image.getAttribute('src') : null;           
-            ads.push({ title, price, date, url, imageSrc });
-        }
-    }
-    return ads;
-}
-
-function getPagination(document, request) {
-    const allPages = document.getElementById('all_pages');
-    let pages = [];
-
-    if (allPages) {   
-        const pagination = allPages.getElementsByTagName('a');
-
-        for (let i = 0; i < pagination.length; i++) {
-            const page = pagination[i];
-            const queryString = page.getAttribute('href');
-            const name = page.text.replace(/\r?\n?/g, '').trim();
-
-            const pageNumber = parsePageNumber(queryString);
-            const isActive = isPageActive(pageNumber, request);
-            pages.push({ name, queryString, pageNumber, isActive })
-        }
-    }
-
-    return pages;
-}
-
-function isPageActive(pageNumber, request) {
-    const urlParts = url.parse(request.url, true);
-    const queryParts = queryString.parse(urlParts.search);
-
-    if (queryParts.o === pageNumber) {
-        return true;
-    } else if (queryParts.o && pageNumber === 1) {
-        return true
-    }
-    return false;
-}
-
-function parsePageNumber(query) {    
-    const queryParts = queryString.parse(query);
-    
-    return queryParts.o;
-    
+    })
+    .catch((error) => console.log(error));
 }
 
 function buildPath(request) {
---
+++ blocket-api/parsing.service.js
@@ -0,0 +1,83 @@
+const url = require('url');
+const queryString = require('query-string');
+
+
+function parseAds(document) {
+    const ads = [];
+    const itemList = document.getElementById('item_list');
+    if (itemList) {
+        const articles = itemList.getElementsByTagName('article');
+        
+        for (let i = 0; i < articles.length; i++) {
+            const article = articles[i];
+            const price = article.getElementsByClassName('list_price')[0].textContent;
+            const date = article.getElementsByTagName('time')[0].textContent;
+            const header = article.getElementsByTagName('h1')[0].getElementsByTagName('a')[0];
+            const title = header.getAttribute('title');
+            const url = header.getAttribute('href');
+
+            const image = article.getElementsByTagName('img')[0];
+            const imageSrc = image ? image.getAttribute('longdesc') || image.getAttribute('src') : null;           
+            ads.push({ title, price, date, url, imageSrc });
+        }
+    }
+    return ads;
+}
+
+function parsePagination(document, request) {
+    const parsedPages = [];
+    const htmlPagination = document.getElementById('all_pages');
+
+    if (htmlPagination) {   
+        const pagination = htmlPagination.getElementsByTagName('a');
+
+        for (let i = 0; i < pagination.length; i++) {
+            const page = pagination[i];
+            const queryString = page.getAttribute('href');
+            const name = page.text.replace(/\r?\n?/g, '').trim();
+            const pageNumber = parsePageNumber(queryString);
+            const isActive = isPageActive(pageNumber, request);
+            
+            parsedPages.push({ name, queryString, pageNumber, isActive });
+        }
+    }
+
+    return parsedPages;
+}
+
+function parseRegions(document) {
+    const regions = [];
+    const htmlRegions = document.getElementsByClassName('region_link_list');
+
+        for (i = 0; i < htmlRegions.length; i++) {
+            const htmlRegion = htmlRegions[i];
+            
+            const name = htmlRegion.text;
+            const href = htmlRegion.getAttribute('href');
+            const regionNumber = htmlRegion.getAttribute('data-region');
+
+            regions.push({ name, href, regionNumber });
+        }
+    
+    return regions;
+}
+
+function parsePageNumber(query) {    
+    const queryParts = queryString.parse(query);
+    
+    return queryParts.o;
+}
+
+function isPageActive(pageNumber, request) {
+    const urlParts = url.parse(request.url, true);
+    const queryParts = queryString.parse(urlParts.search);
+
+    if (queryParts.o === pageNumber) {
+        return true;
+    } else if (queryParts.o && pageNumber === 1) {
+        return true
+    }
+    return false;
+}
+
+module.exports = { parsePagination, parseRegions, parseAds }
