b06a497311b66f45b752af1cf29d22785e21b626
markusaverheim <markus.averheim@gmail.com>
2019-08-06 12:09:59 +0200

    Add cors

--- blocket-api/app.js
+++ blocket-api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const https = require('https');
 const jsdom = require('jsdom');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/', require('./blocket.controller'));
