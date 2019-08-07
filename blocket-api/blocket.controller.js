78d103b333343b87d4bce083b9cd3f489b378014
markusaverheim <markus.averheim@gmail.com>
2019-08-07 14:45:47 +0200

    Move logic from controller to service

--- blocket-api/blocket.controller.js
+++ blocket-api/blocket.controller.js
@@ -1,40 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const url = require('url');
 const blocketService = require('./blocket.service');
-const parserService = require('./parsing.service');
-
 
 router.get('/regions', getRegions);
 router.get('/search', search);
 
 module.exports = router;
 
-
 async function getRegions(request, response) {
-    blocketService.getDocument('https://www.blocket.se').then(document => { // TODO Move url to config
-        const regions = parserService.parseRegions(document);
-
-        response.send(regions);
-    })
-    .catch(error => console.log(error));
+    const regions = await blocketService.getRegions();
+    response.send(regions);
 }
 
-function search(request, response) {
-    const path = buildPath(request);
-
-    blocketService.getDocument(path).then(document => {
-        const ads = parserService.parseAds(document);
-        const pagination = parserService.parsePagination(document, request);
-
-        response.send({ ads, pagination });
-    })
-    .catch((error) => console.log(error));
-}
-
-function buildPath(request) {
-    let basePath = 'https://www.blocket.se/hela_sverige' // TODO Move url to config
-    const urlParts = url.parse(request.url, true);
-
-    return urlParts.search ? `${basePath}${urlParts.search}` : basePath;
+async function search(request, response) {
+    const result = await blocketService.search(request);
+    response.send(result);
 }
--- blocket-api/blocket.service.js
+++ blocket-api/blocket.service.js
@@ -1,6 +1,30 @@
 const jsdom = require('jsdom');
 const https = require('https');
+const parserService = require('./parsing.service');
+const url = require('url');
 
+async function getRegions() {
+    const document = await getDocument('https://www.blocket.se'); // TODO Move url to config
+    const regions = parserService.parseRegions(document);
+
+    return regions;
+}
+
+async function search(request) {
+    const path = buildPath(request);
+    const document = await getDocument(path)
+    const ads = parserService.parseAds(document);
+    const pagination = parserService.parsePagination(document, request);
+
+    return { ads, pagination };
+}
+
+function buildPath(request) {
+    let basePath = 'https://www.blocket.se/hela_sverige' // TODO Move url to config
+    const urlParts = url.parse(request.url, true);
+
+    return urlParts.search ? `${basePath}${urlParts.search}` : basePath;
+}
 
 async function getDocument(url) {
     const promise = new Promise((resolve, reject) => {
@@ -25,4 +49,4 @@ async function getDocument(url) {
     return promise;
 }
 
-module.exports = { getDocument }
+module.exports = { getRegions, search }
